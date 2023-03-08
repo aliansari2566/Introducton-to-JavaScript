719f8ce3e4ac7ee39812818cde694565eb553737
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-08 17:07:48 +0500

    strings in js

--- intro to js/loops.js
+++ intro to js/loops.js
@@ -27,7 +27,20 @@ for (let i = 0; i < cars.length; i++) {
 console.log(text, + ' ');
 
 
+let str = '';
 
+for (let i = 0; i < 9; i++) {
+  str = str + i;
+  console.log(str + ' ');
+ 
+}
+// parseInt(str);
+// console.log(parseInt(typeof str));
+console.log(str + ' ');
+console.log(typeof str );
+
+Number(str);
+console.log(typeof Number(str));
 
 
 // objects with for loop
@@ -100,17 +113,3 @@ do {
 counter++;
 }while(counter < n)
 
-let str ='';
-console.log(str + ' this is first ');
-for (let i = 0; i < 9; i++) {
-  str = str + i;
-  console.log(str + ' ');
- 
-}
-parseInt(str);
-// console.log(parseInt(typeof str));
-console.log(str + ' ');
-console.log(typeof str );
-
-Number(str);
-console.log(typeof  Number(str));
