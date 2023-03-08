ab927ac7a6e0e470f74cbbed46d11677737f1249
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-08 17:08:05 +0500

    Update strings.js

---
+++ intro to js/strings.js
@@ -0,0 +1,25 @@
+
+// Template literals Backtick in js `````   
+// String interpolation : inserting varialbe directly in template literals 
+
+let boy1 = "Ali";
+let boy2 = "Anis";
+
+// let sentence =`boy1 is friend of boy2  `
+  let sentence =`${boy1} is friend of ${boy2} `  // like this u can print the value of variable in a string
+
+console.log(sentence + '\n ');
+
+// Backtick in js `````   
+
+// String Methods 
+let name = "Anis";
+ let sen = "      th is is my javaScript practice session .  ";
+ console.log(sen);
+console.log(sen.trim()); // remove white spaces
+
+console.log(name.toLocaleUpperCase());
+let new_name = name.replace("Anis","Azee");
+console.log(name.replace("An","Az"));
+console.log(name);
+console.log(new_name);
