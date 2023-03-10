3eb1c44e2ae6800e8b2ddf9c9d8ef04f03d36d03
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-10 17:39:09 +0500

    Update arrays.js

--- intro to js/arrays.js
+++ intro to js/arrays.js
@@ -1,4 +1,18 @@
 
 // Array are mutable arrays can be chnaged
 
-let array_of_marks = [78,82,98,56];
+let array_of_marks = [78,82,98,56,49];
+
+console.log(array_of_marks);// printing full array
+
+
+// printing array by index 
+
+console.log(array_of_marks[0]);
+console.log(array_of_marks[1]);
+console.log(array_of_marks[2]);
+console.log(array_of_marks[3]);
+console.log(array_of_marks[4]);
+console.log(array_of_marks[5]);// will be undefined because there is no index 6 in array
+
+// adding the new value to array 
