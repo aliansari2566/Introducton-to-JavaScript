f07f8349e1b8d791b1d1df6de0e0160e212f2ea2
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-13 18:40:45 +0500

    Update array-methods.js

--- intro to js/array-methods.js
+++ intro to js/array-methods.js
@@ -2,8 +2,16 @@
 
 let numbers = [2,4,55,77];
 //map() ek new array return krta hai array k har element pe operations preform kr k 
- let a = numbers.map((value , index)=>{
+// let a = numbers.map(function (value , index) { below code is also some you can just write function this way
+ let a = numbers.map((value , index) =>{
     console.log(value, index);
-    return value + 1;
+ 
+    if(value%2!=0)
+    {
+       return value+1
+    }
+    else{
+        return value
+    }
  })
-//  console.log(a);
+ console.log(a);
