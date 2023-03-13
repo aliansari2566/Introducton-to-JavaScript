af6189e34227603a242ea2e523378392f6fca95c
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-13 11:03:44 +0500

    Update arrays.js

--- intro to js/arrays.js
+++ intro to js/arrays.js
@@ -89,6 +89,7 @@ console.log("array after splice() \n" + num + " length => " + num.length);
 // delete operator in js 
 
 let numbers_array = [1,2,3,4,5,6,7,8];
+let numbers_arr = [9,10,11,12,13,14,15];
 
 console.log(numbers_array.length);
 
@@ -97,3 +98,6 @@ delete numbers_array[0]; // it does not affect the length of array
 console.log(numbers_array.length);
 
 
+//array concat 
+let new_array = numbers_array.concat(numbers_arr);
+console.log(new_array);
