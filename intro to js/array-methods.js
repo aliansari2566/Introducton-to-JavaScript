76e2f78849c6d1818e3528e36facf87baba8881b
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-13 18:32:21 +0500

    Update array-methods.js

--- intro to js/array-methods.js
+++ intro to js/array-methods.js
@@ -1,8 +1,9 @@
 // Higher order array methods  map filter reduce
 
 let numbers = [2,4,55,77];
+//map() ek new array return krta hai array k har element pe operations preform kr k 
  let a = numbers.map((value , index)=>{
-    console.log(value);
+    console.log(value, index);
     return value + 1;
  })
- console.log(a);
+//  console.log(a);
