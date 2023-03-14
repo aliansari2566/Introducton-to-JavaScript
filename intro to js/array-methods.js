8b0159fb1969513af081d3ee63f8c5d912f29382
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-14 18:03:43 +0500

    Update array-methods.js

--- intro to js/array-methods.js
+++ intro to js/array-methods.js
@@ -45,3 +45,19 @@ let a2 =numbers.filter((we_can_write_anything ,index) => {
 console.log(a2);
 
 
+// reduce retunrs a value 
+
+// The reduce() method executes a reducer function for array element.
+
+// The reduce() method returns a single value: the function's accumulated result.
+
+// The reduce() method does not execute the function for empty array elements.
+
+// The reduce() method does not change the original array.
+
+let a3 = numbers.reduce((value1 , value2) =>{
+
+    return value1 - value2
+})
+
+console.log(a3);
