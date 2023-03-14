fd9bafb17afa069a69b8a5f9ef956a3ee2672275
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-14 18:54:01 +0500

    Create js_basic.js

---
+++ intro to js/js_basic.js
@@ -0,0 +1,75 @@
+console.log("below this ");
+
+
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// The Object Datatype
+// The object data type can contain:
+
+// 1. An object
+// 2. An array
+// 3. A date
+
+let a, b, c;                       // declare 3 variables.
+
+let z = 35;                      // declare and initialize variable z 
+
+
+
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+
+
+
+
+
+
+
+
+
+let array = [23,2,13,17,73,91,23,82];
+
+for (let j=1 ,i = 0; i < array.length; j++  )  {
+
+ 
+  if(array[i]!==array[j])
+  {
+    console.log(array[i] );
+    i++;
+  }
+
+ 
+}
+
+let arr1=[2,4,6,8,9];
+// arr1[2].push(3);
+
+console.log(arr1);
