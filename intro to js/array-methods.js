b2df3ae0f6f2b09a038f0abbc82ed8db92611fd0
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-14 17:20:31 +0500

    Update array-methods.js

--- intro to js/array-methods.js
+++ intro to js/array-methods.js
@@ -3,6 +3,13 @@
 let numbers = [2,4,55,77];
 //map() ek new array return krta hai array k har element pe operations preform kr k 
 // let a = numbers.map(function (value , index) { below code is also some you can just write function this way
+// map() creates a new array from calling a function for every array element.
+
+// map() calls a function once for each element in an array.
+
+// map() does not execute the function for empty elements.
+
+// map() does not change the original array.
  let a = numbers.map((value , index) =>{
     console.log(value, index);
  
