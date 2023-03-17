026422ae18d8ede8bfe39d00f62006a54e350ba0
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-17 16:05:57 +0500

    Update console-objects.js

--- in-browser-js/console-objects.js
+++ in-browser-js/console-objects.js
@@ -1,3 +1,21 @@
 console.log(console); // it will print all the console methods like log() assert() error() so on....
 
 console.log("hi ali"); // in console it will return undefined 
+
+
+let age = parseInt(prompt('Enter the value ')); 
+
+const candrive = (()=>{
+    return age > 18 ? true:false;
+})
+
+if (age<0) {
+    console.error("invalid age");
+
+}
+if (candrive(age)) {
+    alert('you can drive')
+} 
+else{
+    alert('you can  not driver')
+}
