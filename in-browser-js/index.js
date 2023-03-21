331bda7ce75e97a343295271e501dfd0f9df10c2
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-21 18:15:17 +0500

    Update index.js

--- in-browser-js/index.js
+++ in-browser-js/index.js
@@ -1,15 +1,16 @@
-console.log("Hello");
+// console.log("Hello");
 
-function makeid(length) {
-    let result = '';
-    const characters = 'SWG';
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
-    }
-    return result;
-}
+// function makeid(length) {
+//     let result = '';
+//     const characters = 'SWG';
+//     const charactersLength = characters.length;
+//     let counter = 0;
+//     while (counter < length) {
+//       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//       counter += 1;
+//     }
+//     return result;
+// }
 
-console.log(makeid(1));
+// console.log(makeid(1));
+
