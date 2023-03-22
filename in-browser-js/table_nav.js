357dc1b4a26e9bdbeeb86287fab6da5506c5a244
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-22 16:44:53 +0500

    Update table_nav.js

--- in-browser-js/table_nav.js
+++ in-browser-js/table_nav.js
@@ -4,4 +4,4 @@ let a = document.body.firstElementChild.firstElementChild
 console.log("First child of a: " + a.firstChild);
 console.log("First Element Child of a: " + a.firstElementChild);
 console.log("First Element Child of a: " + a.firstElementChild.firstElementChild);
-console.log(a.rows);
+console.log(a.table);
