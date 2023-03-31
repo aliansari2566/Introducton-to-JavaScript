67000cac913f3b415cf33dfc5eb90127191c14d2
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-31 15:50:47 +0500

    Update insert.js

--- in-browser-js/insert.js
+++ in-browser-js/insert.js
@@ -2,3 +2,12 @@
 
 let a = document.getElementsByTagName('div')[1];
 a.innerHTML = a.innerHTML + '<h1>Hello world! </h1> '
+
+
+
+//vanila javscript 
+const element = document.getElementById('div');
+element.innerHTML = 'Hello, world';
+
+//jquery
+$('div').text('Hello, world');
