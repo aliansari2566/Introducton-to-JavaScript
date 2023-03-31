dc49e0c891464efc000775404c056d1625fb581f
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-03-31 16:10:47 +0500

    Update insert.js

--- in-browser-js/insert.js
+++ in-browser-js/insert.js
@@ -1,9 +1,12 @@
 // HTML Insertion Methods 
 
 let a = document.getElementsByTagName('div')[1];
-a.innerHTML = a.innerHTML + '<h1>Hello world! </h1> '
+// a.innerHTML = a.innerHTML + '<h1>Hello world! </h1> '
 
 
+let div = document.createElement('div');
+div.innerHTML + '<h3>Hi world! </h3> ';
+div.appendChild(div)
 
 //vanila javscript 
 const element = document.getElementById('div');
