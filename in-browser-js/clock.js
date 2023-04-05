c79c1230e577d4fcb118a9f60e6ed9780bd48d6d
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-04-05 16:04:55 +0500

    Update clock.js

--- in-browser-js/clock.js
+++ in-browser-js/clock.js
@@ -28,14 +28,3 @@
 // }
 // showTime();
 
-let clock = () => {
-    let date = new Date();
-    let hrs = date.getHours();
-    let mins = date.getMinutes();
-    let secs = date.getSeconds();
-    let time = `${hrs}:${mins}:${secs}`
-    // log the time in console
-    console.log(time)
-  }
-  // call the clock function
-  clock();
