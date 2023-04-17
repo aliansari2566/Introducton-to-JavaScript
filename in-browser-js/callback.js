c02bc46c900edb5f181d3b579542be849dbdd65f
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-04-17 12:23:14 +0500

    Update callback.js

--- in-browser-js/callback.js
+++ in-browser-js/callback.js
@@ -60,30 +60,6 @@ else{
 loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" ,hello)
 
 
-
-const loadScript = (src , callback) => {
-  let script = document.createElement("script");
-  script.src=src;
-  script.onload = function (){
-    console.log("loaded script with src " + src);
-    Callback(null , src);
-  }
-document.body.appendChild(script);
-  
-}
-
-const hello = (error,src )=>{
-if(error){
-  console.log(error);
-  return
-}
-else{
-  alert("Hello world" + src)
-}
-}
-
-
-loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" ,hello)
   // callback hell & pyramid of doom ----------------------------
 
 
