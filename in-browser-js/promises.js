62ef2b45cb88f98f2d3716c18e889d05362a4fad
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-04-20 14:43:42 +0500

    Update promises.js

--- in-browser-js/promises.js
+++ in-browser-js/promises.js
@@ -1,9 +1,13 @@
 let myPromise = new Promise((myResolve, myReject) =>{
     setTimeout(function() { 
-        
+
       console.log("resolved after 2 sec"); }, 2000);
 
-      myResolve(56);
+      myResolve();
   });
 
-  myPromise();
+ myPromise.then((value) =>{
+    console.log(value);
+ }).catch(()=>{
+    console.log("an error occour");
+ })
