c6be7a91b629c3ef26c06833fbbd20d2031cf6be
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-04-28 16:13:32 +0500

    Update promises.js

--- in-browser-js/promises.js
+++ in-browser-js/promises.js
@@ -78,7 +78,7 @@ let p4 = new Promise((Resolve, myReject) =>{
 });
 let p5 = new Promise((Resolve, myReject) =>{
   
-  
+  Resolve("p5");
   // setTimeout(function() { 
   //   Resolve("p5");
   // }, 5000);
