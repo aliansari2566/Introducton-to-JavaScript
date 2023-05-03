ac4ba2f91ced1437927cccc4bdffd9cf949312a3
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-05-03 18:40:13 +0500

    Update error handling.js

--- in-browser-js/error handling.js
+++ in-browser-js/error handling.js
@@ -7,5 +7,14 @@ setTimeout(() => {
 try {
 console.log(Ali);    
 } catch (error) {
-    
+    console.log("Error occur on try");
 }
+setTimeout(() => {
+    console.log("Fetching user details.......................................");
+     
+ }, 2000);
+
+setTimeout(() => {
+    console.log("Fetching username and password ***********************...........");
+     
+ }, 2000);
