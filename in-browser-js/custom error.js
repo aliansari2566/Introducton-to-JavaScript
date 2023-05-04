7813f4464bb1a75367497f04d695aab5b444e30c
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-05-04 16:25:05 +0500

    Update custom error.js

--- in-browser-js/custom error.js
+++ in-browser-js/custom error.js
@@ -1,7 +1,20 @@
+//builtin erros------------------
+
 try {
     ali
 } catch (error) {
     console.log(error.message);
     console.log(error.name);
-    
+    console.log(error.stack);
+
+}
+
+//custom errors 
+try {
+    throw new error("something went wrong")
+} catch (error) {
+    console.log(error.message);
+    console.log(error.name);
+    console.log(error.stack);
+
 }
