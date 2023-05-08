c5d56f5648bbba7acd07a319657725560626ee3f
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-05-08 14:36:35 +0500

    Update loadscript.js

--- in-browser-js/loadscript.js
+++ in-browser-js/loadscript.js
@@ -10,12 +10,19 @@ const loadScript = async(src) => {
     
   }) 
 
-}
-const main = async () =>{
 
-console.log(new Date().getMilliseconds);
-let a = await loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" )
-console.log(a);
-console.log(new Date().getMilliseconds);
 }
-main();
+
+let a = loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" )
+a.then( (value)=>{
+console.log(value);
+})
+
+// const main = async () =>{
+
+// console.log(new Date().getMilliseconds);
+// let a = await loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" )
+// console.log(a);
+// console.log(new Date().getMilliseconds);
+// }
+// main();
