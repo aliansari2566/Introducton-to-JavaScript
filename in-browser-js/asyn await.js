286e578a0151599ed94cb4f76e3ed8534f2b8009
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-05-08 15:04:48 +0500

    Create asyn await.js

---
+++ in-browser-js/asyn await.js
@@ -0,0 +1,23 @@
+const loadScript = async(src) => {
+
+    return myPromise = new Promise((Resolve, myReject) =>{
+    let script = document.createElement("script");
+    script.src=src;
+    script.onload =  () =>{
+      Resolve(src);
+    }
+    document.head.append(script)
+    
+  }) 
+
+
+}
+
+const main = async () =>{
+
+console.log(new Date().getMilliseconds);
+let a = await loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" )
+console.log(a);
+console.log(new Date().getMilliseconds);
+}
+main();
