f4e45cdbe5d64c08a7412d6815b0fdeec82afa69
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-05-08 14:36:55 +0500

    Update loadscript.js

--- in-browser-js/loadscript.js
+++ in-browser-js/loadscript.js
@@ -13,6 +13,7 @@ const loadScript = async(src) => {
 
 }
 
+//Load script with promise
 let a = loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" )
 a.then( (value)=>{
 console.log(value);
