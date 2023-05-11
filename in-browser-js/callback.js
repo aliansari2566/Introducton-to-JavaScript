432534cb9b78e679f1d62447033db59ebbccd91e
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-05-11 14:25:42 +0500

    Update callback.js

--- in-browser-js/callback.js
+++ in-browser-js/callback.js
@@ -34,27 +34,26 @@ function myDisplayer(some) {
 
 // one more example of callback -------------
 
-const gift =(callback)=>{
+const getGift =(callback)=>{
 setTimeout(() => {  // pr ye jo anonymous function pass kia ha settime out mein ye 3000 second bad run hoga  ga or is se phly gitf() wala function run ho chuka hoga us mein koi value nahi hogi
-  const get_gift="🎁";
-console.log("got the gift" + get_gift);
+  const gift="🎁";
+console.log("got the gift" + gift);
 //  return gift; but this line will not return gift function ne 3000 seconds k bad gift ki value return krni hai abhi kch bhi nhi retunr kry ga apka gift ka function to foran execute hojye
 
 
 // we have to use a callback function  which will wait for the gift and when its return  ye hmy callback kr de ga 
-callback(get_gift); // ab ye gift k return hone ka wait kry ga 
+callback(gift); // ab ye gift k return hone ka wait kry ga 
 
 }, 3000);
 }
 
 
+//   console.log("got the gift before callback " + getGift());
 
+getGift((gift)=>{
 
-
-
-
-
-
+  console.log("got the gift after 2 sec " + gift);
+})
 
 
 // one more example of callback -------------
