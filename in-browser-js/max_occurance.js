770058951f5f287bfba4da64d02f230d77a3319e
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-05-12 18:26:43 +0500

    Create max_occurance.js

---
+++ in-browser-js/max_occurance.js
@@ -0,0 +1,37 @@
+
+// let str = "this sa string programmers often like to avoid complex code";
+let str1 = "this is a string programmers often like to avoid comple";
+
+
+
+let str = "sample str ing ";
+
+// console.log("Max occurring character is: " + getMaxOccurringChar(str));
+
+
+// const MaxOccurring_ =(Str)=>{  // map empty hai 
+//     const map={}
+//     for(let i=0 ;i<str.length ; i++){
+//     str.split("").map[i]=map[i]?map[i]+1:1;
+//         console.log(map);
+    
+    
+//     }
+    
+    
+//}
+
+const MaxOccurring =(Str)=>{
+const map={}
+str.split("").forEach(element => {
+    map[element]=map[element]?map[element]+1:1;
+    console.log(element);
+});
+
+
+
+}
+MaxOccurring (str1)
+
+
+
