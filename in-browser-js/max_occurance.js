83fd34a4c83af0d467aaaae4a46811bbe6d8726f
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-05-15 11:39:27 +0500

    Update max_occurance.js

--- in-browser-js/max_occurance.js
+++ in-browser-js/max_occurance.js
@@ -6,7 +6,6 @@ let str1 = "this is a string programmers often like to avoid comple";
 
 let str = "sample str ing ";
 
-// console.log("Max occurring character is: " + getMaxOccurringChar(str));
 
 
 // const MaxOccurring_ =(Str)=>{  // map empty hai 
@@ -21,8 +20,12 @@ let str = "sample str ing ";
     
 //}
 
+// console.log("Max occurring character is: " + MaxOccurring_(str));
+
+
+
 const MaxOccurring =(Str)=>{
-const map={}
+const map= new Map()
 str.split("").forEach(element => {
     map[element]=map[element]?map[element]+1:1;
     console.log(element);
