f9368f91096a4daecf2c31bc6ec35dcc9eeb93ef
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-05-16 18:59:38 +0500

    Update max_occurance.js

--- in-browser-js/max_occurance.js
+++ in-browser-js/max_occurance.js
@@ -7,12 +7,10 @@ let str1 = "this is a string programmers often like to avoid comple";
 
 
 
-// const MaxOccurring_ =(Str)=>{  // map empty hai 
+// const MaxOccurring_ =(Str)=>{  
 //     const map={}
 //     for(let i=0 ;i<str.length ; i++){
-//     str.split("").map[i]=map[i]?map[i]+1:1;
-//         console.log(map);
-    
+//    
     
 //     }
     
@@ -21,39 +19,60 @@ let str1 = "this is a string programmers often like to avoid comple";
 
 // console.log("Max occurring character is: " + MaxOccurring_(str));
 
+
+
 let str = "saemeple strinng 1233322 124./;'";
 
 const MaxOccurring = (Str)=>{
-const map = new Map()
-str.split("").forEach(element => {
-    map[element]=map[element]?map[element]+1:1;
+const map = {};
+
+console.log(map);
+
+
+
+// map={ key : value}
+
+str.split("").map(element => {
+
+    map[element] =  map[element]?map[element]+1:1;
+    console.log(map);
+    // if (map[element]) {
+       
+    //   map[element] + 1
+    //   console.log(element);
+    // }
+    // else{
+    //   map[element]= 1
+    // }
+    // console.log(map);
+    // console.log("type of element" + typeof(element) , typeof(map[element]));
+    // console.log("after map");
+    // console.log(element);
     // console.log(element);
 });
 
-let char = str[0];
-let max =1;
+// let char = str[0];
+// let max =1;
 
-  for (let i in map) {
-    // console.log("map " + i + " " + map[i]);
+//   for (let i in map) {
+//     // console.log("map " + i + " " + map[i]);
  
-    if (i !== '' && map[i] > max) {
+//     if (i !== '' && map[i] > max) {
     
-        max =map[i]
-        char =i;
-    }
-    // else if(map[i] > max){
-      
-    //     max =map[i]
-    //     char =i;
-    // }
-  
-  }
+//         max =map[i]
+//         char =i;
+//     }
+   
+//   }
 
-// return char,max;
-console.log(" max occurring char = " + char + " occurance = "+ max);
+// // return char,max;
+// console.log(" max occurring char = " + char + " occurance = "+ max);
 
 
 }
+
+
+
  MaxOccurring (str);
 // console.log(max_char);
 
