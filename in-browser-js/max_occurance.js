361e8fd6cf0bf346955bb5cc411c4662f5462ea8
Muhammad Ali Hamza <aliansari2566@gmail.com>
2023-05-22 17:54:20 +0500

    Update max_occurance.js

--- in-browser-js/max_occurance.js
+++ in-browser-js/max_occurance.js
@@ -19,62 +19,71 @@ let str1 = "this is a string programmers often like to avoid comple";
 
 // console.log("Max occurring character is: " + MaxOccurring_(str));
 
-
-
 let str = "saemeple strinng 1233322 124./;'";
 
 const MaxOccurring = (Str)=>{
-const map = {};
-
-console.log(map);
-
-
-
-// map={ key : value}
-
-str.split("").map(element => {
-
-    map[element] =  map[element]?map[element]+1:1;
-    console.log(map);
-    // if (map[element]) {
-       
-    //   map[element] + 1
-    //   console.log(element);
-    // }
-    // else{
-    //   map[element]= 1
-    // }
-    // console.log(map);
-    // console.log("type of element" + typeof(element) , typeof(map[element]));
-    // console.log("after map");
-    // console.log(element);
+const map = new Map()
+str.split("").forEach(element => {
+    map[element]=map[element]?map[element]+1:1;
     // console.log(element);
 });
 
-// let char = str[0];
-// let max =1;
+let char = str[0];
+let max =1;
 
-//   for (let i in map) {
-//     // console.log("map " + i + " " + map[i]);
+  for (let i in map) {
+    // console.log("map " + i + " " + map[i]);
  
-//     if (i !== '' && map[i] > max) {
+    if (i !== '' && map[i] > max) {
     
-//         max =map[i]
-//         char =i;
-//     }
-   
-//   }
+        max =map[i]
+        char =i;
+    }
+    // else if(map[i] > max){
+      
+    //     max =map[i]
+    //     char =i;
+    // }
+  
+  }
 
-// // return char,max;
-// console.log(" max occurring char = " + char + " occurance = "+ max);
+// return char,max;
+console.log(" max occurring char = " + char + " occurance = "+ max);
 
 
 }
-
-
-
  MaxOccurring (str);
 // console.log(max_char);
+
+
+// more than 2 character with the maximum occurrence  
+
+// const MaxOccurring = (str) => {
+//   const charCounts = new Map();
+//   let maxCount = 0;
+//   let maxChars = [];
+
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str[i];
+//     charCounts.set(char, (charCounts.get(char) || 0) + 1);
+
+//     const count = charCounts.get(char);
+
+//     if (count > maxCount) {
+//       maxCount = count;
+//       maxChars = [char];
+//     } else if (count === maxCount) {
+//       maxChars.push(char);
+//     }
+//   }
+
+//   console.log("Max occurring char(s): " + maxChars.join(', '));
+//   console.log("Occurrence count: " + maxCount);
+// };
+
+// const str1 = "saemeple strinng  vvv./;'";
+// MaxOccurring(str);
+
 
 
 
